0adfe7d7847458e7d981194cd0069e80ccb525d2
Matt <metwas.seup@Gmail.com>
2026-03-27 16:39:54 +0000

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -10,7 +10,38 @@ sudo pacman -S --needed --noconfirm mesa
 sudo pacman -S --needed --noconfirm xf86-video-amdgpu
 sudo pacman -S --needed --noconfirm vulkan-radeon
 sudo pacman -S --needed --noconfirm libva-mesa-driver
+# Update
+sudo pacman -Sc --noconfirm
+sudo pacman-key --populate archlinux
+sudo pacman -Sy --noconfirm archlinux-keyring
+sudo pacman -Syu --noconfirm
+sudo pacman -Sy --needed --noconfirm iwd
+sudo pacman -S --needed --noconfirm wine winetricks sdl2-compat gst-plugins-base-libs gst-plugins-good gst-plugins-bad gst-plugins-ugly ffmpeg samba gnutls lib32-alsa-oss
 sudo pacman -S --needed --noconfirm pipewire pipewire-pulse pipewire-alsa lib32-pipewire lib32-pipewire-pulse lib32-libpulse lib32-alsa-lib lib32-alsa-plugins
+sudo pacman -S --needed --noconfirm vulkan-radeon lib32-vulkan-radeon mesa lib32-mesa
+sudo pacman -S --needed --noconfirm --needed gcc-libs lib32-gcc-libs glibc lib32-glibc
+sudo pacman -S --needed --noconfirm \
+  webkit2gtk-4.1 \
+  base-devel \
+  curl \
+  wget \
+  file \
+  openssl \
+  appmenu-gtk-module \
+  libappindicator-gtk3 \
+  librsvg \
+  xdotool
+
+# Hyprland base
+sudo pacman -S --needed --noconfirm hyprland
+sudo pacman -S --needed --noconfirm rofi-wayland
+sudo pacman -S --needed --noconfirm hyprpaper
+sudo pacman -S --needed --noconfirm hyprlock
+sudo pacman -S --needed --noconfirm xdg-desktop-portal-hyprland
+sudo pacman -S --needed --noconfirm xorg-xwayland
+
+# Tools
+sudo pacman -S --needed --noconfirm nmap
 
 # Apps
 yay -S --needed --noconfirm brave-bin
